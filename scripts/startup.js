fbae1848335cd44ac52668efcecae64a2a757db9
bsmithAppliedMemetics <b.smith@AppliedMemeticsllc.com>
2017-10-02 20:29:50 -0400

    shell push

--- scripts/startup.js
+++ scripts/startup.js
@@ -3,4 +3,16 @@ module.exports = function(r2d2) {
 	r2d2.hear(/alive?/, function(res) {
  		return res.send(":+1:");
 	});   
+
+	r2d2.respond(/food/, function(res){
+		res.reply "what do you want?";
+
+		r2d2.hear(/chipotle/, function(res){
+			res.reply "Burrito?";
+			r2d2.hear(/yes/, function(res){
+				r2d2.reply "Ordered!";
+			});
+		});
+
+	})
  }
