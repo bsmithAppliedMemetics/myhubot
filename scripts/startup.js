59d358a74adc20d5eb6c5a5404699a7f10b8236f
bsmithAppliedMemetics <b.smith@AppliedMemeticsllc.com>
2017-10-15 15:50:35 -0400

    shell push

--- scripts/startup.js
+++ scripts/startup.js
@@ -26,6 +26,9 @@ module.exports = function(r2d2) {
 		menu = true;
 	});
 
+	r2d2.hear(places[1], function(res){
+		res.reply("Eww Gross");
+	});
 	r2d2.hear(/Chipotle/, function(res){
 		if(menu){
 			restraunt = 'Chipotle';
